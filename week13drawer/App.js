add5068b1fe2e6044a37c7e5962140be9591293d
reversed-quikz <transformersah@live.com>
2021-04-29 14:36:20 -0400

    Update App.js
    updated for 3 new components using state

--- week13drawer/App.js
+++ week13drawer/App.js
@@ -19,9 +19,22 @@ function HomeScreen(props) {
     title="Laps Swam"
     onPress={() => props.navigation.navigate('Laps')}
   />
+  <Button 
+    title="Push Ups"
+    onPress={() => props.navigation.navigate('PushUp')}
+  />
+  <Button 
+    title="Sit Ups"
+    onPress={() => props.navigation.navigate('SitUp')}
+  />
+  <Button 
+    title="Jumping Jacks"
+    onPress={() => props.navigation.navigate('JumpingJack')}
+  />
   </View>
   );
 }
+
 function CurlScreen(props){
   const [curls, setCurls] = useState(0);
     return(
@@ -38,6 +51,58 @@ function CurlScreen(props){
         </View>
     );
 }
+
+function PushUpScreen(props){
+  const [pushup, setPushups] = useState(0);
+    return(
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Push Ups: {pushup}</Text>
+            <Button title="Add Push Up"
+            onPress={() => setPushups(pushup + 1)}/>
+            <Button title="Reset Counter" 
+            onPress={() => setPushups(0)}/>
+            <Button
+        title="Back to Home"
+        onPress={() => props.navigation.navigate('Home')}
+      />
+        </View>
+    );
+}
+
+function SitUpScreen(props){
+  const [situp, setSitups] = useState(0);
+    return(
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Sit Ups: {situp}</Text>
+            <Button title="Add Sit Up"
+            onPress={() => setSitups(situp + 1)}/>
+            <Button title="Reset Counter" 
+            onPress={() => setSitups(0)}/>
+            <Button
+        title="Back to Home"
+        onPress={() => props.navigation.navigate('Home')}
+      />
+        </View>
+    );
+}
+
+function JumpingJackScreen(props){
+  const [jj, setJJs] = useState(0);
+    return(
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Jumping Jacks: {jj}</Text>
+            <Button title="Add Rep"
+            onPress={() => setJJs(jj + 1)}/>
+            <Button title="Reset Counter" 
+            onPress={() => setJJs(0)}/>
+            <Button
+        title="Back to Home"
+        onPress={() => props.navigation.navigate('Home')}
+      />
+        </View>
+    );
+}
+
 class LapsScreen extends React.Component {
       constructor(props) {
         super(props);
@@ -61,6 +126,7 @@ render() {
     );
 }
 }
+
 const Drawer = createDrawerNavigator();
 function App() {
   return (
@@ -69,6 +135,9 @@ function App() {
         <Drawer.Screen name="Home"component={HomeScreen}/>
         <Drawer.Screen name="Curl" component={CurlScreen}/>
         <Drawer.Screen name="Laps" component={LapsScreen}/>
+        <Drawer.Screen name="PushUp"component={PushUpScreen}/>
+        <Drawer.Screen name="SitUp" component={SitUpScreen}/>
+        <Drawer.Screen name="JumpingJack" component={JumpingJackScreen}/>
       </Drawer.Navigator>
    </NavigationContainer>
   );
